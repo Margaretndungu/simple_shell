1d728276ad9a146de35721d3c34b4a9d6eae5ca4
Omokehinde happiness <omokehindehappiness589@gmail.com>
2023-07-20 21:39:05 +0000

    a shell that displays prompt for a user read & execute the command

--- shell.c
+++ shell.c
@@ -1,24 +1,24 @@
 #include "main.h"
 
 /**
-  * prompt_user - prompt the user to imput a command
+  * prompt_user_file1 - prompts a user
   *
-  * Return: Nothing
+  * Reurn: Nothing
   */
 
-void prompt_user(void)
+void prompt_user_file1(void)
 {
 	write(STDOUT_FILENO, ":) ", 3);
 }
 
 /**
-  * read_line - reads the user imput command
+  * read_line_file1 - reads user input command
   *
-  * Return: the number of characters read, including delimiter character
+  * Return: number of characters read, including the delimeter character
   * -1 on failure
   */
 
-char *read_line()
+char *read_line_file1(void)
 {
 	char *line = NULL;
 	size_t bufsize = 0;
@@ -28,7 +28,7 @@ char *read_line()
 	{
 		if (line)
 			free(line);
-		write(STDOUT_FILENO. "\n", 1);
+		write(STDOUT_FILENO, "\n", 1);
 		return (NULL);
 	}
 
@@ -37,21 +37,37 @@ char *read_line()
 }
 
 /**
-  * execute_command - execute a command using execve function
+  * execute_command_file1 - executes a command using execve function
   * @command: command to be executed
   *
   * Return: Nothing
   * -1 on failure
   */
 
-void execute_command(char *command)
+void execute_command_file1(char *command)
 {
 	pid_t pid = fork();
+	int exec_result;
 
 	if (pid == -1)
 	{
 		perror("fork");
 		exit(EXIT_FAILURE);
+	}
+	else if (pid == 0)
+	{
+		char *args[2];
+
+		args[0] = command;
+		args[1] = NULL;
+
+		exec_result = execve(command, args, NULL);
+
+		if (exec_result == -1)
+		{
+			perror("execve");
+		}
+		exit(EXIT_SUCCESS);
 	}
 	else
 	{
@@ -61,26 +77,4 @@ void execute_command(char *command)
 	}
 }
 
-/**
-  * main - entry point
-  *
-  * Return: Always 0 (success)
-  */
 
-int main(void)
-{
-	char *line;
-
-	while (1)
-	{
-		prompt_user();
-		line = read_line();
-
-		if (line == NULL)
-			break;
-
-		execute_command(line);
-		free(line);
-	}
-	return (0);
-}
